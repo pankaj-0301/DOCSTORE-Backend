62e6b06c312cda63c0616b51bafbb551d5bf6ac2
Pankaj Paliwal <158048038+pankaj-0301@users.noreply.github.com>
2024-09-19 15:49:21 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+// const dotenv = require('dotenv');
 
 // Load environment variables from .env file
-dotenv.config();
+// dotenv.config();
 
 // Get MongoDB URI from environment variables
-const uri = process.env.MONGO_URI;
+const uri = MONGO_URI;
 
 const connectDB = async () => {
   try {
